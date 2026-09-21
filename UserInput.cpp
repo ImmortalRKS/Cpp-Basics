082a72497d7fba793925b8648b297df7372e8fee
ImmortalRKS <rishitks1307@gmail.com>
2026-09-21 21:23:33 +0530

    userinput

---
+++ UserInput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a {0};
+    int b {0};
+
+    cout << "Enter values for adding : ";
+    cin >> a;
+    cin >> b;
+    cout << a+b;
+
+    return 0;
+
+}
