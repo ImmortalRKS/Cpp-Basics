665b0d83970eb80a4731ac980da84872e6a2f0c4
ImmortalRKS <rishitks1307@gmail.com>
2026-09-21 20:46:04 +0530

    Constants

---
+++ Constant.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double pi = 3.14; //const doesnt allow to change the value
+    cout << pi;
+    return 0;
+}
+
+// Different Naming conventions
+int hello_world = 0; //snake case
+int Hello_World = 0; //pascal case
+int hello_World = 0; //camel case
